51bf85910e32c4fb3b570b412731eb4867493380
tech nerd <yeabnoah5@gmail.com>
2024-07-18 04:27:45 +0300

    finished doc for the NextJs starter

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,26 +1,24 @@
 // import connectDB from "@/lib/mongodb";
 // import User from "@/model/user";
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// // --- simple Get request Example
-// export const GET = async (request: NextRequest) => {
-//   await connectDB(); // connect to Database
+// --- simple Get request Example
+export const GET = async (request: NextRequest) => {
+  // await connectDB(); // connect to Database
+  // const allUsers = await User.find();
+  // return NextResponse.json(allUsers);
+};
 
-//   const allUsers = await User.find();
+// --- simple POST request example
+export const POST = async (request: NextRequest) => {
+  // await connectDB(); // connect to Database
 
-//   return NextResponse.json(allUsers);
-// };
+  const body = await request.json();
+  const { username, password } = body;
 
-// // --- simple POST request example
-// export const POST = async (request: NextRequest) => {
-//   await connectDB(); // connect to Database
+  //   Example of saving user to MongoDB using Mongoose
+  // const newUser = new User({ username, password });
+  // await newUser.save();
 
-//   const body = await request.json();
-//   const { username, password } = body;
-
-//   //   Example of saving user to MongoDB using Mongoose
-//   const newUser = new User({ username, password });
-//   await newUser.save();
-
-//   return NextResponse.json({ username, password });
-// };
+  return NextResponse.json({ username, password });
+};
